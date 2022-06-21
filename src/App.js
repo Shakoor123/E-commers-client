32f24dc81eb08c513fa7e5dd09907d8ed22891e9
shakoor123 <shakoorchachu2001@gmail.com>
2022-06-21 22:19:27 +0530

    home page created success using mui and dummy data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Anouncement from './components/Anouncement/Anouncement';
-import Navbar from './components/Navbar/Navbar';
-import Slider from './components/Slider/Slider';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-       <Anouncement/>
-      <Navbar/>
-      <Slider/>
+      <Home/>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import  Announcement from '../components/Anouncement/Anouncement'
 import React from 'react'
+import Navbar from "../components/Navbar/Navbar"
+import Categories from '../components/Categories/Categories'
+import Products from '../components/Products/Products'
+import Footer from '../components/footer/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Announcement/>
+      <Navbar/>
+      <Categories/>
+      <Products/>
+      <Footer/>
+    </div>
   )
 }
 
